dc0e94f0ec467e71b1a33935bb56b5de4941d168
Imran Hossen Bappy <61227100+imranBappy@users.noreply.github.com>
2022-06-04 01:18:06 +0600

    decimal to hex function done

--- templates/scripts/Coverter.js
+++ templates/scripts/Coverter.js
@@ -22,9 +22,42 @@ class Converter {
 
     function decimalConverter(value, base) {
       let result = "";
-      while (value > 0) {
-        result = (value % base) + result;
-        value = Math.floor(value / base);
+      if (base === 16) {
+        while (value > 0) {
+          let digit = value % base;
+          if (digit < 10) {
+            result = digit + result;
+          } else {
+            switch (digit) {
+              case 10:
+                result = "A" + result;
+                break;
+              case 11:
+                result = "B" + result;
+                break;
+              case 12:
+                result = "C" + result;
+                break;
+              case 13:
+                result = "D" + result;
+                break;
+              case 14:
+                result = "E" + result;
+                break;
+              case 15:
+                result = "F" + result;
+                break;
+              default:
+                break;
+            }
+          }
+          value = Math.floor(value / base);
+        }
+      } else {
+        while (value > 0) {
+          result = (value % base) + result;
+          value = Math.floor(value / base);
+        }
       }
       return result;
     }
